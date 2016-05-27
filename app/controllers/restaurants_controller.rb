c68610d4b3e5aca23ff57d5d33231ca14b70d90a
Czarek <karasoi@poczta.onet.pl>
2016-05-27 14:33:55 +0200

    Add json rendering to restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,10 @@ class RestaurantsController < ApplicationController
 
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to @restaurant
+      respond_to do |format|
+        format.html {render :show}
+        format.json { render @restaurant}
+      end
     else
       render 'new'
     end
@@ -19,13 +22,17 @@ class RestaurantsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @restaurants }
+      format.json
     end
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @comments = @restaurant.comments
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render @restaurant}
+    end
   end
 
 
---
+++ app/views/restaurants/_restaurant.json.jbuilder
@@ -0,0 +1,1 @@
+json.(restaurant, :id, :name, :description, :latitude, :longitude)
